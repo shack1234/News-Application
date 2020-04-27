59118173947a468555469126229e24ee190587d2
shack1234 <shadrack.anayo@strathmore.edu>
2020-04-27 13:39:33 +0300

    added base_url

--- config.py
+++ config.py
@@ -1,13 +1,10 @@
 import os
-
 class Config:
     '''
     General configuration parent class
     '''
     NEWS_API_BASE_URL = 'https://newsapi.org/v2/sources?language=en&apiKey='
-
     ARTICLES_API_BASE_URL ='http://newsapi.org/v2/top-headlines?sources={}&apiKey={}'
-
     NEWS_API_KEY = os.environ.get('NEWS_API_KEY')
    
 class ProdConfig(Config):
